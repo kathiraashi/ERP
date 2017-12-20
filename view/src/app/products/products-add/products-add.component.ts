f9f4be2f0acd6e8588fa008d6879aaf66d7de036
kathiraashi <kathiraashi@gmail.com>
2017-12-20 09:42:25 +0530

    InitialCommit

--- view/src/app/products/products-add/products-add.component.ts
+++ view/src/app/products/products-add/products-add.component.ts
@@ -1,6 +1,6 @@
 // default modules
 import { Component, OnInit, TemplateRef  } from '@angular/core';
-import {FormBuilder, FormGroup, Validators, FormControl, FormArray} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormControl,  FormArray} from '@angular/forms';
 import { DefaultUrlHandlingStrategy } from '@angular/router/src/url_handling_strategy';
 
 //Feture Modules
@@ -30,7 +30,6 @@ export class ProductsAddComponent implements OnInit {
   newCar: boolean;
   msgs: Message[] = [];
   rowId;
-  deleteId;
 
 
   //modal 
@@ -40,7 +39,7 @@ export class ProductsAddComponent implements OnInit {
   ProductAddForm: FormGroup;
   AttrAddForm: FormGroup;
 
-  invoiceForm: FormGroup;
+  public invoiceForm: FormGroup;
 
   productTypes = [ 'Stockble', 'Consumable', 'Service'];
   unitofMeasures = [ 'Unit of Measures-One', 'Unit of Measures-Two', 'Unit of Measures-Three'];
@@ -48,10 +47,10 @@ export class ProductsAddComponent implements OnInit {
   vin;
 
   constructor(  private formBuilder: FormBuilder,
-                private _formBuild: FormBuilder,
                 private modalService: BsModalService,
                 private carService: CarService,
-                private messageService: MessageService 
+                private messageService: MessageService,
+                private _fb: FormBuilder 
               ) {
     this.createForm();
     this.addForm();
@@ -60,49 +59,35 @@ export class ProductsAddComponent implements OnInit {
   ngOnInit() {
     this.carService.getCarsSmall().then(cars => this.cars = cars);
 
-    this.invoiceForm = this._formBuild.group({
-      Package_Title: [''],
-      HotelData: this._formBuild.array([this.addRows()])
-    });
 
+    this.invoiceForm = this._fb.group({
+      itemRows: this._fb.array([this.initItemRows()])
+    });
   }//ngOnInit
 
 
 
-  addRows() {
-    let group = this._formBuild.group({
-      Htitle: [''],
-      HDescription: [''],
-      hotelStar: [],
-      RoomData: this._formBuild.array([])
+  initItemRows() {
+    return this._fb.group({
+      attrname: [''],
+      attrvalue:['']
     });
-    this.addRoom(group.controls.RoomData)
-    return group;
-  }
-  
-  addHotel() {
-    const control: FormArray = this.invoiceForm.get(`HotelData`) as FormArray;
-    control.push(this.addRows());
-  }
-  
-  addRoom(hotel:any) {
-    let group = this._formBuild.group({
-      Hotel_Room_Type: ['']
-    })
-    hotel.push(group)
-  }
-  
-  removeRooms(hotel, index) { 
-    hotel.controls.RoomData.removeAt(index)
-    
-  }
+}
+
+addNewRow() {
+    const control = <FormArray>this.invoiceForm.controls['itemRows'];
+    control.push(this.initItemRows());
+}
+
+deleteRow(index: number) {
+    const control = <FormArray>this.invoiceForm.controls['itemRows'];
+    control.removeAt(index);
+}
 
 
 
 
   addAttributeAdd(){
-    this.AttrAddForm.reset();
-    this.AttrAddForm.markAsUntouched();
     this.car = new PrimeCar();
     this.newCar = true;
   }
@@ -110,6 +95,7 @@ export class ProductsAddComponent implements OnInit {
   arttributeEdit(id){
     this.rowId = id;
     this.car = this.cars[id];
+    console.log(this.car);
     this.newCar = false;
   }
 
@@ -117,25 +103,28 @@ export class ProductsAddComponent implements OnInit {
     console.log('submit');
   }
 
-  formSubmit(){ 
+  formSubmit(){
+    let cars = [...this.cars];
     if(!this.newCar){
-      let cars = [...this.cars];
-      this.cars[this.rowId] = this.car;
-      this.cars= cars;
+      this.cars[this.rowId]=this.car;
+      console.log(this.car);
     }else{
+      console.log(this.car);
+     this.cars= cars;
      this.cars.push( Object.assign({}, this.car));
-     this.cars = this.cars.slice();
+     this.cars=this.cars.slice();
     }
-  }
-  
-  openModal(roindex, template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-    this.deleteId = roindex;
+    
   }
 
-  confirmDelete(index): void {
-    console.log(index);
-    this.cars = this.cars.filter((val,i) => i!=index);
+  
+
+  
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirmDelete(): void {
     this.modalRef.hide();
     this.msgs = [];
     this.msgs.push({severity:'success', summary:'Alert Message', detail:'Deleted'});
@@ -165,11 +154,15 @@ export class ProductsAddComponent implements OnInit {
   };
   addForm(){
     this.AttrAddForm= this.formBuilder.group({ 
-      vin: ['', Validators.compose([ ])],
-      year:  ['', Validators.compose([ ])],
-      color: ['', Validators.compose([ ])],
-      brand:  ['', Validators.compose([ ])]
+      vin: ['', Validators.compose([ Validators.required ])],
+      year:  ['', Validators.compose([ Validators.required ])],
+      color: ['', Validators.compose([ Validators.required ])],
+      brand:  ['', Validators.compose([ Validators.required ])]
     });
+
+    this.invoiceForm= this.formBuilder.group({ 
+      Package_Title: ['', Validators.compose([ Validators.required ])]
+      });
 
   };//createForm
   
