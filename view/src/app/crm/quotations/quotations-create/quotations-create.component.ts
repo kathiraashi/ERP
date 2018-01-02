2597e666f3916fe873e749bcf8d8101305afee48
kathiravan <kathiraashi@gmail.com>
2018-01-02 19:30:05 +0530

    Initial Commit

--- view/src/app/crm/quotations/quotations-create/quotations-create.component.ts
+++ view/src/app/crm/quotations/quotations-create/quotations-create.component.ts
@@ -21,23 +21,38 @@ import { QuoteProductFormComponent } from '../../../popups/crm/quote-product-for
 })
 export class QuotationsCreateComponent implements OnInit {
 
+  showFinalTab:boolean = false;
     msgs: Message[] = [];
     //modal 
     modalRef: BsModalRef;
 
   QuoteForm: FormGroup;
+  DummyValidateForm: FormGroup;
+  QuoteTermsForm: FormGroup;
 
   // material dialog 
   QuoteProductDialogRef: MatDialogRef<QuoteProductFormComponent>;
   products = [];
   product;
+  showDiscount:boolean = false;
   updateIndex;
-  deleterowIndex
+  deleterowIndex;
+  total = 0;
+  totalDiscount = 0;
+  totalTax =0;
+  totalAll = 0;
+  subTotal = '0';
+  discountTotal = '0';
+  taxTotal = '0';
+  Total = '0';
+  termsValue;
 
   CompanyNames = [ 'Company Name 1', 'Company Name 2', 'Company Name 3'];
   ContactPersons = [ 'Contact Person 1', 'Contact Person 2', 'Contact Person 3'];
   OpportunityNames = [ 'Opportunity Name 1', 'Opportunity Name 2', 'Opportunity Name 3'];
   EmployeeNames = [ 'Employee Name 1', 'Employee Name 2', 'Employee Name 3'];
+
+  QuoteTermsOptions = [ 'QuoteTermsOption-One', 'QuoteTermsOption-Two', 'QuoteTermsOption-Three', 'QuoteTermsOption-Four'];
 
   constructor( 
                 public dialog: MatDialog,
@@ -51,13 +66,20 @@ export class QuotationsCreateComponent implements OnInit {
   createForm(){
     this.QuoteForm = new FormGroup({
         date: new FormControl('', Validators.required),
-        companyName: new FormControl('', Validators.required),
-        contactPerson: new FormControl('',  Validators.required),
-        quoteRefNo: new FormControl('',  Validators.required),
-        validTill: new FormControl('', Validators.required),
-        opportunityName: new FormControl('', Validators.required),
-        employeeName: new FormControl('', Validators.required)
+        companyName: new FormControl(''),
+        contactPerson: new FormControl(''),
+        quoteRefNo: new FormControl(''),
+        validTill: new FormControl(''),
+        opportunityName: new FormControl(''),
+        employeeName: new FormControl('')
       });
+    this.DummyValidateForm = new FormGroup({
+        produtsEmpty: new FormControl('', Validators.required)
+      });
+    this.QuoteTermsForm = new FormGroup({
+      termsOption: new FormControl('', Validators.required),
+      terms: new FormControl('', Validators.required)
+    });
       
   }
 
@@ -66,28 +88,75 @@ export class QuotationsCreateComponent implements OnInit {
 // Forms Popups
     //Quotation Product Add
         QuoteProductAdd() {
-          let QuoteProductDialogRef = this.dialog.open(QuoteProductFormComponent, { width:'75%', data: { Header:'Quotation Product Add Form', type:'Add' } });
+          let QuoteProductDialogRef = this.dialog.open(QuoteProductFormComponent, { width:'75%', disableClose:true,  data: { Header:'Quotation Product Add Form', type:'Add' } });
           QuoteProductDialogRef.afterClosed().subscribe(result => this.newproductAdd(result));
         }//QuoteProductAdd
         QuoteProductEdit(index) {
           this.product = this.products[index];
           this.updateIndex = index;
-          let QuoteProductDialogRef = this.dialog.open(QuoteProductFormComponent, { width:'75%', data: { Header:'Quotation Product Edit Form', type:'Edit', value:this.product } });
+          let QuoteProductDialogRef = this.dialog.open(QuoteProductFormComponent, { width:'75%', disableClose:true, data: { Header:'Quotation Product Edit Form', type:'Edit', value:this.product } });
           QuoteProductDialogRef.afterClosed().subscribe(result => this.updateproductAdd(result));
         }//QuoteProductEdit
 
         newproductAdd(result){
-          console.log(result);
           if(result !== "Closed"){
             this.products.push( Object.assign({}, result));
             this.products = this.products.slice();
+            this.getTotal();
           }
         }
         updateproductAdd(result){
           if(result !== "Closed"){
             this.products[this.updateIndex] = result;
             this.products = this.products.slice();
+            this.getTotal();
           }
+       }
+       getTotal() {
+          this.total = 0; 
+          this.totalDiscount = 0;
+          this.totalTax = 0;
+          this.totalAll = 0;
+          let discountShowOption = 0;
+
+          if(this.products.length > 0){
+              this.products.forEach((product) => {
+                    if (parseInt(product.subtotal) > 0){ 
+                            this.total = this.total + Number(product.subtotal); 
+                            this.subTotal = (this.total).toFixed(2);
+                          }else{  this.subTotal = '0';   }
+                    if (parseInt(product.completediscountAmount) > 0){ 
+                            this.totalDiscount = this.totalDiscount + Number(product.completediscountAmount); 
+                            this.discountTotal = (this.totalDiscount).toFixed(2);
+                            discountShowOption = 1;
+                          }else{  this.discountTotal = '0';  }
+                    if (parseInt(product.completetax) > 0){ 
+                            this.totalTax = this.totalTax + Number(product.completetax); 
+                            this.taxTotal = (this.totalTax).toFixed(2);
+                          }else{  this.taxTotal = '0'; }
+                    if (parseInt(product.completetotal) > 0){ 
+                            this.totalAll = this.totalAll + Number(product.completetotal); 
+                            this.Total = (this.totalAll).toFixed(2);
+                          }else{  this.Total = '0'; }
+              });
+
+              if(discountShowOption === 1){
+                this.showDiscount = true;
+              }else{
+                this.showDiscount = false;
+              }
+
+              this.DummyValidateForm.controls['produtsEmpty'].setValue('Not Empty');
+          }
+          else{
+            this.subTotal = '0';
+            this.discountTotal = '0';
+            this.taxTotal = '0';
+            this.Total = '0';
+            this.DummyValidateForm.controls['produtsEmpty'].setValue('');
+          }
+
+
        }
 
 
@@ -103,26 +172,21 @@ export class QuotationsCreateComponent implements OnInit {
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
   }
-
-  
-
   deleteId(rowid){
     this.deleterowIndex = rowid;
   }
-
   confirmDelete(rowid): void {
     this.products = this.products.filter((val,i) => i!=rowid);
+    this.getTotal();
     this.modalRef.hide();
     this.msgs = [];
     this.msgs.push({severity:'warn', summary:'Alert Message', detail:'Deleted'});
   }
-
   declineDelete(): void {
     this.modalRef.hide();
     this.msgs = [];
     this.msgs.push({severity:'success', summary:'Alert Message', detail:'Declined'});
   }
-
 }
 
 
--- view/src/app/popups/crm/quote-product-form/quote-product-form.component.ts
+++ view/src/app/popups/crm/quote-product-form/quote-product-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit, Injectable } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA  } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA, JAN  } from '@angular/material';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';;
 
 import {MatChipInputEvent} from '@angular/material';
@@ -38,9 +38,10 @@ export class QuoteProductFormComponent implements OnInit {
   enterdPrice:number;
   enterdQuantity:number;
   enterdDiscount:number;
+  discountAmount;
   multiplePrice:number;
   taxamount:number;
-  priceTotal:number;
+  taxTotal:number;
   taxget:number;
   finalTotal:number;
   
@@ -51,44 +52,56 @@ export class QuoteProductFormComponent implements OnInit {
       private customeService: CustomService,
       private formBuilder: FormBuilder,
       private dialogRef: MatDialogRef<QuoteProductFormComponent>,
-      @Inject(MAT_DIALOG_DATA) private data:any ) { }
+      @Inject(MAT_DIALOG_DATA) private data:any ) { 
+
+        this.customeService.getProductCustom().then(product => this.products = product);
+        this.customeService.getTaxCustom().then(tax => this.taxes = tax);
+
+      }
   
 
-    ngOnInit() {
-      this.customeService.getProductCustom().then(product => this.products = product);
-      this.customeService.getTaxCustom().then(tax => this.taxes = tax);
-
+  ngOnInit() {
       this.QuoteProductForm = new FormGroup({
           product: new FormControl('', Validators.required),
           description: new FormControl({value: '', disabled: true}, Validators.required),
           price: new FormControl('', Validators.required),
           quantity: new FormControl('1', Validators.required),
-          tax: new FormControl('', Validators.required),
-          completetotal: new FormControl({value: '', disabled: true}, Validators.required),
-          discount: new FormControl('', Validators.required)
+          tax: new FormControl(''),
+          subtotal: new FormControl({value: '', disabled: true}),
+          completetotal: new FormControl({value: '', disabled: true}),
+          completetax: new FormControl({value: '', disabled: true}),
+          discount: new FormControl('0'),
+          completediscountAmount: new FormControl(''),
+          totaltaxamount: new FormControl('')
       });
 
       if (this.data.type == 'Edit') {
-          if(this.data.value.discount !== ""){ this.discountDisabled = true; };
-          this.selectedTaxes = this.data.value.tax;
+          if(this.data.value.discount !== ""){ 
+            this.discountDisabled = true;
+            this.discountAmount = this.data.value.completediscountAmount;  
+          };
+          if(this.data.value.tax) { 
+            this.selectedTaxes = this.data.value.tax;
+          }
           this.QuoteProductForm.controls['product'].setValue(this.data.value.product);
           this.QuoteProductForm.controls['description'].setValue(this.data.value.description);
           this.QuoteProductForm.controls['price'].setValue(parseInt(this.data.value.price));
           this.QuoteProductForm.controls['quantity'].setValue(parseInt(this.data.value.quantity));
           this.QuoteProductForm.controls['discount'].setValue(parseInt(this.data.value.discount));
-          this.QuoteProductForm.controls['completetotal'].setValue(parseInt(this.data.value.completetotal));
+          this.QuoteProductForm.controls['completediscountAmount'].setValue(this.data.value.completediscountAmount);
+          this.QuoteProductForm.controls['subtotal'].setValue(this.data.value.subtotal);
+          this.QuoteProductForm.controls['completetax'].setValue(this.data.value.completetax);
+          this.QuoteProductForm.controls['completetotal'].setValue(this.data.value.completetotal);
         
       }
-
-
   }
+
 
   clickEvent(){
     this.discountDisabled = !this.discountDisabled;
-    if(this.discountDisabled === false){
       this.QuoteProductForm.controls['discount'].setValue('');
-      this.calculate();
-    }    
+      this.discountAmount = 0;
+      this.calculate(); 
   }
 
   productsSearchKey(value: string){
@@ -110,6 +123,16 @@ export class QuoteProductFormComponent implements OnInit {
 //Search Tax List
     taxSearchKey(){
       this.taxKey = this.QuoteProductForm.value.tax ;  
+
+      if(this.data.type === "Edit" && this.data.value.tax){
+        this.selectedTaxes.forEach((taxvalue) => {
+          this.filterTaxType = taxvalue.tax_computation;
+          this.tax = this.taxes.filter(x => x.id == taxvalue.id);
+          let taxesIndex = this.taxes.indexOf(this.tax[0]);
+          if (taxesIndex >= 0) { this.taxes.splice(taxesIndex, 1); }
+        })
+      }
+
       if(this.taxKey !== ""){
 
         if(this.filterTaxType !== ""){
@@ -160,10 +183,12 @@ export class QuoteProductFormComponent implements OnInit {
       this.enterdDiscount = this.QuoteProductForm.value.discount;
       this.multiplePrice = this.enterdPrice * this.enterdQuantity;
       if(this.enterdDiscount > 0){
-        this.multiplePrice = this.multiplePrice - ( (this.multiplePrice * this.enterdDiscount) / 100 ) ;
-      } 
+        this.discountAmount = ((this.multiplePrice * this.enterdDiscount) / 100).toFixed(2);
+        this.QuoteProductForm.controls['completediscountAmount'].setValue(this.discountAmount);
+        this.multiplePrice = this.multiplePrice - ( (this.multiplePrice * this.enterdDiscount) / 100 ) ; 
+      }else{ this.discountAmount = 0; this.QuoteProductForm.controls['completediscountAmount'].setValue(''); }
       this.finalTotal = this.multiplePrice;
-      this.priceTotal = 0;
+      this.taxTotal = 0;
       this.taxamount = 0;
       
       if(this.enterdPrice > 0 && this.enterdQuantity > 0){
@@ -172,33 +197,44 @@ export class QuoteProductFormComponent implements OnInit {
                 this.taxget = parseInt(datas.amount);
                 if(datas.tax_computation === "Fixed"){
                     this.finalTotal = (this.finalTotal + this.taxget);
+                    this.taxTotal = (this.taxTotal + this.taxget);
                 }
                 if(datas.tax_computation === "Percentage Of Price"){
                     this.taxamount = (this.multiplePrice * this.taxget) / 100;
                     this.finalTotal = ( this.finalTotal + this.taxamount);
+                    this.taxTotal = (this.taxTotal + this.taxamount);
                 }
                 if(datas.tax_computation === "Percentage Of (Price + Tax)"){
                       this.taxamount = (this.finalTotal * this.taxget) / 100;
-                      this.finalTotal = (this.finalTotal + this.taxamount); 
+                      this.finalTotal = (this.finalTotal + this.taxamount);
+                      this.taxTotal = (this.taxTotal + this.taxamount);
                 }
                 if(datas.tax_computation === "Percentage Of Price Tax Included"){
                       if(this.taxamount > 0){
                           this.taxamount = (this.taxamount + (this.multiplePrice - (this.multiplePrice / (1 + (this.taxget/100)))));
+                          this.taxTotal = (this.taxTotal + this.taxamount);
                       }else{
                           this.taxamount = this.multiplePrice - (this.multiplePrice / (1 + (this.taxget/100)));
+                          this.taxTotal = (this.taxTotal + this.taxamount);
                       }
                 }
             });
 
           if(this.filterTaxType === "Percentage Of Price Tax Included"){
-            this.QuoteProductForm.controls['completetotal'].setValue(this.multiplePrice);
+            this.QuoteProductForm.controls['subtotal'].setValue((this.multiplePrice - this.taxTotal).toFixed(2));
+            this.QuoteProductForm.controls['completetotal'].setValue(this.multiplePrice.toFixed(2));
+            this.QuoteProductForm.controls['completetax'].setValue(this.taxTotal.toFixed(2));
           }else{
-            this.QuoteProductForm.controls['completetotal'].setValue(this.finalTotal);
+            this.QuoteProductForm.controls['subtotal'].setValue(this.multiplePrice.toFixed(2));
+            this.QuoteProductForm.controls['completetotal'].setValue(this.finalTotal.toFixed(2));
+            this.QuoteProductForm.controls['completetax'].setValue(this.taxTotal.toFixed(2));
           }
           
                     
       }
       else{
+        this.QuoteProductForm.controls['subtotal'].setValue('');
+        this.QuoteProductForm.controls['completetax'].setValue('');
         this.QuoteProductForm.controls['completetotal'].setValue('');
         }
 
@@ -215,8 +251,10 @@ export class QuoteProductFormComponent implements OnInit {
     }
 
     submit() {
-      this.QuoteProductForm.controls['tax'].setValue(this.selectedTaxes );
-      this.dialogRef.close(this.QuoteProductForm.getRawValue());
+      if(this.QuoteProductForm.valid && this.QuoteProductForm.value.price > 0 && this.QuoteProductForm.value.quantity > 0){
+        this.QuoteProductForm.controls['tax'].setValue(this.selectedTaxes );
+        this.dialogRef.close(this.QuoteProductForm.getRawValue());
+      }
     }
 
 }
