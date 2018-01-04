d6b0a37adcf806e8968e010ff595cf7ad406021a
kathiravan <kathiraashi@gmail.com>
2018-01-04 13:27:17 +0530

    Initial Commit

--- view/src/app/crm/sale-orders/sale-orders-create/sale-orders-create.component.ts
+++ view/src/app/crm/sale-orders/sale-orders-create/sale-orders-create.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl,  FormArray} from '@angular/forms';
+import { DefaultUrlHandlingStrategy } from '@angular/router/src/url_handling_strategy';
+import { Router, ActivatedRoute } from '@angular/router';
+
+//Feture Modules
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
+//services
+import { DataSharedService } from '../../../service/DataSharedService';
 
 @Component({
   selector: 'app-sale-orders-create',
@@ -7,9 +17,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaleOrdersCreateComponent implements OnInit {
 
-  constructor() { }
+  products=[];
+
+  PO_confirm_Show = false;
+  email_confirm_Show = false;
+  phone_confirm_Show = false;
+
+  SaleOrderForm: FormGroup;
+  DummyValidateForm: FormGroup;
+
+
+  orderConfirmations = [ 'Purchase Order', 'E-mail','Over a Phone'];
+
+  constructor( 
+                private sharedService:DataSharedService,
+                private formBuilder: FormBuilder,
+                private modalService: BsModalService,
+                private route: ActivatedRoute, 
+                private router: Router
+              ) { this.createForm(); }
+
+                
+
+  createForm(){
+    this.SaleOrderForm = new FormGroup({
+        salesNumber: new FormControl({value:'SaleOrder-001', disabled:true}),
+        customer: new FormControl({value:'Company Name', disabled:true}),
+        quoteRefNo: new FormControl({value:'Quote-003', disabled:true}),
+        contactPerson: new FormControl({value:'Contact Person Name', disabled:true}),
+        validTill: new FormControl({value:'12/01/2018', disabled:true}),
+        salesPerson: new FormControl({value:'Employee Name', disabled:true}),
+        orderConfirmedBy: new FormControl(''),
+        confirmationDate: new FormControl(''),
+        poNumber: new FormControl(''),
+        poDate: new FormControl(''),
+        poDetails: new FormControl('')
+      });
+    this.DummyValidateForm = new FormGroup({
+        produtsEmpty: new FormControl('', Validators.required)
+      });
+      
+  }
+
+
+  
+// Forms Popups
+
 
   ngOnInit() {
+    // this.carService.getCarsSmall().then(cars => this.cars = cars);
+  }
+
+  OrderConfirmChange(value){
+    if(value === "Purchase Order"){ 
+        this.PO_confirm_Show = true;
+        this.email_confirm_Show = false;
+        this.phone_confirm_Show = false;
+      }
+    if(value === "E-mail"){ 
+        this.email_confirm_Show = true;
+        this.PO_confirm_Show = false;
+        this.phone_confirm_Show = false;
+    }
+    if(value === "Over a Phone"){ 
+        this.phone_confirm_Show = true;
+        this.PO_confirm_Show = false;
+        this.email_confirm_Show = false;
+      }
+  }
+
+
+  onsubmit(){
+   let returnPage = this.sharedService.GetreturnPage();
+   if(returnPage !== ""){
+    this.sharedService.SetreturnPage("");
+    this.router.navigate([returnPage]);
+   }else{
+    this.router.navigate(['crmQuotationsList']);
+   }
   }
 
 }
--- view/src/app/crm/sale-orders/sale-orders-list/sale-orders-list.component.ts
+++ view/src/app/crm/sale-orders/sale-orders-list/sale-orders-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+//Feture Modules
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
 //custome Modules
 import { Car} from '../../../domain/car';
 import { CarService} from '../../../service/carservice';
@@ -16,12 +20,40 @@ export class SaleOrdersListComponent implements OnInit {
 
   cars: Car[];
   selectedCar1: Car;
+  ShowQuotes:boolean = true;
+  orderConfirmations = [ 'Purchase Order', 'E-mail','Over a Phone'];
+  customers = [ 'Company Name 1', 'Company Name 2', 'Company Name 3'];
+  quotes = [ 'Quote-001', 'Quote-002', 'Quote-003',  'Quote-004'];
 
-  constructor(private sharedService:DataSharedService, private carService: CarService, private route: ActivatedRoute, private router: Router) { }
+  //modal 
+  modalRef: BsModalRef;
+
+  constructor(
+    private modalService: BsModalService,
+    private sharedService:DataSharedService, 
+    private carService: CarService, 
+    private route: ActivatedRoute, 
+    private router: Router) { }
 
   ngOnInit() {
     this.carService.getCarsMedium().then(cars => this.cars = cars);
   }
+
+  createSale(){
+    
+  }
+
+  SelectCustomer(data){
+    if(data !== ""){
+      this.ShowQuotes = false;
+    }
+  }
+
+  goToSaleCreate(){
+    this.router.navigate(['crmCreateSaleOrders']);
+  }
+
+
 
   ViewQuote(data){
     this.sharedService.saveData(data);
